a203a181ccfb04881776b41d0fa0c94e080a3873
TanakaSei <seiryuu0330@gmail.com>
2022-10-21 01:24:24 +0900

    ヘッダーの編集中

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,17 @@
 <template>
-    <h1>App page</h1>
-    hoge
-    <br>
-    {{hoge}}
-    <br>
-    <a-button type="primary">Primary Button</a-button>
+    <a-layout id="app-layout" style="background: #fff;">
+        <a-layout-header style="background: #ebc320;">
+            <a-row type=" flex">
+                <a-col justify="start">
+                    <a-typography-text strong style="color: #af1e23;">まかないセレクター</a-typography-text>
+                </a-col>
 
+            </a-row>
+        </a-layout-header>
+    </a-layout>
 </template>
 <script>
 import { defineComponent, reactive, ref } from 'vue'
-
 export default defineComponent({
     setup() {
         return {
