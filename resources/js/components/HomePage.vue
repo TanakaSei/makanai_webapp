8cb3137e43237b7ed6a08f0336d60ecf5a808024
TanakaSei <seiryuu0330@gmail.com>
2022-10-22 18:00:30 +0900

    a-card テスト

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -1,5 +1,18 @@
 <template>
     <a-typography-title :level="2">Home Page</a-typography-title>
+    <a-card title="メニュー一覧" style="width: 300px">
+        <template #extra><a href="#">more</a></template>
+        <p>card content</p>
+        <p>card content</p>
+        <p>card content</p>
+    </a-card>
+    <br />
+    <a-card title="最近選んだメニュー" style="width: 300px">
+        <template #extra><a href="#">more</a></template>
+        <p>card content</p>
+        <p>card content</p>
+        <p>card content</p>
+    </a-card>
 </template>
    
 <script>
