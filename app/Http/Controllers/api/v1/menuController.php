78f90c0e5e0ab64c19b648c93f1b18c48f7c4ae9
TanakaSei <seiryuu0330@gmail.com>
2022-10-25 15:37:20 +0900

    メニューマイグレーションファイル作成

--- app/Http/Controllers/api/v1/menuController.php
+++ app/Http/Controllers/api/v1/menuController.php
@@ -6,6 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class menuController extends Controller
-{
-    //
+{   
+    
+    @param Request $request
+    @return void
+
+    public function create(Request $request){
+        $member = Menber::create([
+            'menu_name' => $request->menu_name,
+            'category' => $request->category,
+        ]);
+
+        return responce(->json($menber))
+    }
+
+    @param Request $request
+    @return void
+    
+    public function fetch(Request $request){
+
+    }
+
 }
