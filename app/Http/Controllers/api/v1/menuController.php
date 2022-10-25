8f24bc8fbbfa08fdd89067e85cbcb7e6b3d5c54f
TanakaSei <seiryuu0330@gmail.com>
2022-10-26 00:49:50 +0900

    vueからのパラメータ受け取りのベース作成

--- app/Http/Controllers/api/v1/menuController.php
+++ app/Http/Controllers/api/v1/menuController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Menu;
+use App\Models\Category;
 class menuController extends Controller
 {   
-    public function index(){
-        $menus = menu::all();
+    public function index(Request $request){
+        $category = category::all();
+        $hoge = (int) $request->contents_limit;
+        $menus = menu::find($hoge);
         return response()->json($menus);
     }
 }
-
-
