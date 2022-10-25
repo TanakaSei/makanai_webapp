0af5ff10f5e01ed092397ad7ad06635d26d2dd7a
TanakaSei <seiryuu0330@gmail.com>
2022-10-25 18:50:20 +0900

    menusテーブルの全件をjsonで返すapi完成

--- app/Http/Controllers/api/v1/menuController.php
+++ app/Http/Controllers/api/v1/menuController.php
@@ -4,27 +4,13 @@ namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Menu;
 class menuController extends Controller
 {   
-    
-    @param Request $request
-    @return void
-
-    public function create(Request $request){
-        $member = Menber::create([
-            'menuMame' => $request->menuName,
-            'category' => $request->category,
-        ]);
-
-        return responce(->json($menber))
+    public function index(){
+        $menus = menu::all();
+        return response()->json($menus);
     }
-
-    @param Request $request
-    @return void
-    
-    public function fetch(Request $request){
-
-    }
-
 }
+
+
