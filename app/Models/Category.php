01858858f18be97aee73d9ecfeece77026ce39bd
TanakaSei <seiryuu0330@gmail.com>
2022-10-26 23:36:44 +0900

    メニュー一覧とカテゴリの内部連結準備

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Menu;
 class Category extends Model
 {
     use HasFactory;
+    public function menu()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
