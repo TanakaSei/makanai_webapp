2e5e52cab23e3f7b8d104c29ea225da265e739ba
TanakaSei <seiryuu0330@gmail.com>
2022-10-27 17:26:54 +0900

    検索文字の有無による場合分け

--- app/Http/Controllers/api/v1/menuController.php
+++ app/Http/Controllers/api/v1/menuController.php
@@ -28,14 +28,33 @@ class menuController extends Controller
             $seach_text = (string)$request->seach_text;
         }
         else{
-            $seach_text = '';
+            $seach_text = null;
         }
-        $end_id = $offset+$contents_limit;
+        $end_id = $offset+$contents_limit-1;
+        //seach_textが入力されているかつoffsetからend_idの間に該当項目がなければ該当なしになる
+        //->テーブル全体で一致しているレコードがあっても検索できない
+        //
+        /*内部連結し
+        seach_text空なら，offsetからend_idまでのレコード取得
+        または seach_text空でないなら 最大contents_limit個の一致するレコード取得
+        */
+
+        $menus = Category::join('menus',function($join)use($offset, $end_id, $seach_text){
+            $join->on('menus.category_id', '=','categories.id')
+            ->when((!is_null($seach_text) && $seach_text != ''), function($q) use ($seach_text){
+                $q->where('menuName', 'like', "%$seach_text%");
+            })
+            ->when((is_null($seach_text) || $seach_text ==''), function($q) use ($offset, $end_id){
+                $q->whereBetween('menus.id', [$offset, $end_id]);        
+            });
+        })->get();
+        /*
         $menus = Category::join('menus',function($join)use($offset, $end_id, $seach_text){
             $join->on('menus.category_id', '=','categories.id')
             ->whereBetween('menus.id', [$offset, $end_id])
             ->where('menuName', 'like', "%$seach_text%");
         })->get();
+        */
         return response()->json($menus);
     }
 }
