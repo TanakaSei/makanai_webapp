007bb856e928df7cee2dd34e237f307857b43043
TanakaSei <seiryuu0330@gmail.com>
2022-11-19 22:03:50 +0900

    構文ミス修正

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Menu;
-use UserCategory
+use UserCategory;
 class Category extends Model
 {
     use HasFactory;
--- resources/js/Pages/SettingPage.vue
+++ resources/js/Pages/SettingPage.vue
@@ -105,7 +105,7 @@ export default defineComponent({
             console.log("sava_change!!", select_num, typeof select_num);
             axios.post('api/setting/save', {
                 params: {
-                    id: user_id,
+                    id: 1,
                     select_num: select_num,
                 },
             })
